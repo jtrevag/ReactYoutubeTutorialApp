d17983045485c6ba0d0b6f7d773dce3ce0405447
jtrevag <jtrevag@gmail.com>
2016-01-13 08:51:59 -0600

    Updating to catch up desktop

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = "AIzaSyB4xqmebeaPE7glVab9qKy5C4TXcH4w6JY";
 
 // Create a new component. This component should produce 
 // some HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // Take this component's generated HTML and put it
