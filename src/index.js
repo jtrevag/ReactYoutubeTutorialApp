ca19c11a12ffb46b30c86149108cafc840275fba
jtrevag <jtrevag@gmail.com>
2016-01-13 16:58:20 -0600

    Updated to watch a video!!!

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 const API_KEY = "AIzaSyB4xqmebeaPE7glVab9qKy5C4TXcH4w6JY";
 
 class App extends Component {
@@ -21,6 +22,7 @@ class App extends Component {
     return (
         <div>
             <SearchBar />
+            <VideoDetail video={this.state.videos[0]}/>
             <VideoList videos={this.state.videos} />
         </div>
     );
